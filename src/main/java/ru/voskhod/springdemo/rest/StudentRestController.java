05c2ea4b71c6bcbc13afa87c01ce5c73536f9bba
Александр Васильков <y915@yandex.ru>
2020-01-26 19:23:35 +0300

    Add exception handling

--- src/main/java/ru/voskhod/springdemo/rest/StudentRestController.java
+++ src/main/java/ru/voskhod/springdemo/rest/StudentRestController.java
@@ -1,9 +1,8 @@
 package ru.voskhod.springdemo.rest;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 import ru.voskhod.springdemo.entity.Student;
 
 import javax.annotation.PostConstruct;
@@ -34,7 +33,45 @@ public class StudentRestController {
     // define endpoint for "/students/{studentId}" - return student by id
     @GetMapping("/students/{studentId}")
     public Student getStudent(@PathVariable int studentId) {
+
+        // check studentId against list size
+
+        if ((studentId >= students.size()) || (studentId < 0)) {
+            throw new StudentNotFoundException("Student id not found: " + studentId);
+        }
+
         return students.get(studentId);
+    }
+
+    // Add an exception handler using @ExceptionHandler
+    @ExceptionHandler
+    public ResponseEntity<StudentErrorResponse> handleException(StudentNotFoundException e) {
+
+        // create StudentErrorResponse
+
+        StudentErrorResponse errorResponse = new StudentErrorResponse();
+
+        errorResponse.setStatus(HttpStatus.NOT_FOUND.value());
+        errorResponse.setMessage(e.getMessage());
+        errorResponse.setTimeStamp(System.currentTimeMillis());
+
+        // return ResponseEntity
+
+        return new ResponseEntity<>(errorResponse, HttpStatus.NOT_FOUND);
+    }
+
+    // add another exception handler ... to catch any other exceptions
+    @ExceptionHandler
+    public ResponseEntity<StudentErrorResponse> handleException(Exception e) {
+        StudentErrorResponse errorResponse = new StudentErrorResponse();
+
+        errorResponse.setStatus(HttpStatus.BAD_REQUEST.value());
+        errorResponse.setMessage("Bad request: malformed request payload");
+        errorResponse.setTimeStamp(System.currentTimeMillis());
+
+        // return ResponseEntity
+
+        return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
     }
 
 
